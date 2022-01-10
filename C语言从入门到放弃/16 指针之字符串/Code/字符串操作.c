bb63104bc2cc619c20001e9bbdba4e0f7a257643
maye <zcmaye@gmail.com>
2022-01-10 14:43:10 +0800

    字符串操作

---
+++ C语言从入门到放弃/16 指针之字符串/Code/字符串操作.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+
+//删除字符串中的指定字符
+void removeChar(char* str, char ch)
+{
+	int i, j;
+	for ( i=j = 0; str[i]; i++)
+	{
+		if (str[i] != ch)
+		{
+			str[j++] = str[i];
+		}
+	}
+	str[j] = '\0';
+}
+
+int main()
+{
+	char str[] = "aabbcc";
+	removeChar(str, 'b');
+	puts(str);
+
+
+	return 0;
+}
